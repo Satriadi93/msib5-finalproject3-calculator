bccfc1e7f7cec399b0605640d3f0ab68a417a227
Janwar Ksatria Pamungkas <janwarksatriapamungkas@hotmail.com>
2023-10-31 18:56:25 +0700

    Setel Layout Kalkulator,Setel Status bar Menjadi Transparant

--- app/src/main/java/com/hactiv8/calculator3/MainActivity.java
+++ app/src/main/java/com/hactiv8/calculator3/MainActivity.java
@@ -1,14 +1,24 @@
 package com.hactiv8.calculator3;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
     }
 }
